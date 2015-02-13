1f05fac681480cb19029572cd337b174218b475d
Lyra-B <glykeriapeppa@gmail.com>
2015-02-13 17:23:24 +0000

    Added Images and Class Book

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,3 +1,12 @@
-function handleResponse(response) {
-  console.log(response.items);
+function handleResponse(response){
+  //console.log(response.items);
+  _.each(response.items, function(i) {
+    //var imageLink = i.volumeInfo.imageLinks.thumbnail;
+    var view = _.template("<img src= '<%= image %>' />");
+    var html = view({ image: i.volumeInfo.imageLinks.thumbnail});
+    var imgElement = $(html);
+    $("#bookshelf").append(imgElement);
+    $("img").addClass("book");
+
+  })
 }
