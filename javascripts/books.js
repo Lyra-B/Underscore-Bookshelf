08ab13b4045ee88d447610cbc26a83084b9ee641
Lyra-B <glykeriapeppa@gmail.com>
2015-02-14 00:16:13 +0000

    Wrapped Images in Divs

--- javascripts/books.js
+++ javascripts/books.js
@@ -2,11 +2,12 @@ function handleResponse(response){
   //console.log(response.items);
   _.each(response.items, function(i) {
     //var imageLink = i.volumeInfo.imageLinks.thumbnail;
-    var view = _.template("<img src= '<%= image %>' />");
+    var view = _.template("<div><img src= '<%= image %>' /></div>");
+    // var div = _.template("<div>'<%= view %>'</div>")
     var html = view({ image: i.volumeInfo.imageLinks.thumbnail});
     var imgElement = $(html);
     $("#bookshelf").append(imgElement);
-    $("img").addClass("book");
+    $("div").addClass("book");
   })
 
   _.filter(response.items, function(i){
