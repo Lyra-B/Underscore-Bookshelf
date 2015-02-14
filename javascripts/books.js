f83e9e842fbcd63ed6ff499960cdef8c19a7ffc9
Lyra-B <glykeriapeppa@gmail.com>
2015-02-14 16:37:22 +0000

    Hot Class and span in appropriate divs of class book

--- javascripts/books.js
+++ javascripts/books.js
@@ -7,8 +7,8 @@ function handleResponse(response){
     var html = view({ image: i.volumeInfo.imageLinks.thumbnail});
     var imgElement = $(html);
     $("#bookshelf").append(imgElement);
-    $("div").addClass("book");
-  })
+    $("#bookshelf div").addClass("book");
+  });
 
   // var spanF = function(){
   //   var starTempl = _.template("<span class='ui-icon ui-icon-star'></span>");
@@ -19,26 +19,38 @@ function handleResponse(response){
 
   var highRated =_.filter(response.items, function(i){
     var rating = i.volumeInfo.averageRating;
-    var highRated = [];
     if (rating==4 || rating==5){
-      i;
-    };
-  })
+      return i;
+    }
+  });
 
-  _.each(highRated, function(i){
-    $(i.book).append("<span class='ui-icon ui-icon-star'></span>");
-  })
+  _.each (highRated, function(k){
+    // console.log("");
+    var book_array = $('.book')
+    for(var i = 0; i < book_array.length; i++) {
+      console.log("");
+      if ($(book_array[i]).find("img").attr("src") === k.volumeInfo.imageLinks.thumbnail){
+        $(book_array[i]).addClass("hot");
+      }
+    }
+  });
+  $(".hot").append("<span class='ui-icon ui-icon-star'></span>");
+
+  // if (.book).children()
+  // _.each(highRated, function(i){
+  //
 
 
     // $("span").addClass("ui-icon ui-icon-star")
 
 
-  // Inspired by the the urls below:
+  // Inspired by the the following urls:
   //http://stackoverflow.com/questions/6354149/css-divs-overlapping-how-do-i
   //-force-one-above-the-other
   //http://api.jqueryui.com/zIndex/
   //http://philipwalton.com/articles/what-no-one-told-you-about-z-index/
   //http://api.jqueryui.com/theming/icons/
+  //http://api.jquery.com/attribute-equals-selector/
 
 
 }
